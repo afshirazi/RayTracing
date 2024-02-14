527b5083b5a23d31405a3f743b4273f33123cd8a
afshirazi <ashirazi@purdue.edu>
2024-02-13 20:12:31 -0500

    renders ovals successfully

--- RayTracing/main.cpp
+++ RayTracing/main.cpp
@@ -5,9 +5,9 @@
 #include "Vec3.h"
 
 int width, height;
-std::vector<Vec3> circles;
+std::vector<Vec3 *> circles;
 
-Vec3 get_ray(int x, int y)
+Vec3 get_ray(int x, int y, int width, int height)
 {
     Vec3 eye(0, 0, 0);
     Vec3 lookAt(0, 0, -1);
@@ -15,10 +15,10 @@ Vec3 get_ray(int x, int y)
 
     Vec3 l = Vec3::normalize(lookAt - eye);
     Vec3 v = Vec3::normalize((l.cross(up)));
-    Vec3 u = v.cross(l);
+    Vec3 u = Vec3::normalize(v.cross(l));
 
-    Vec3 ll = eye + (l * 0.4142135624) - v - u;
-    Vec3 p = ll + (v * 2 * x) + (u * 2 * y);
+    Vec3 ll = eye + (l ) - v - u;
+    Vec3 p = ll + (v * 2 * ((double)x / width)) + (u * 2 * ((double)y / height));
     Vec3 dir = Vec3::normalize((p - eye));
 
     return (dir - eye);
@@ -26,26 +26,31 @@ Vec3 get_ray(int x, int y)
 
 bool is_intersect(const Vec3& cent, const Vec3& d)
 {
-    double b = 2 * (d.x * (0 - cent.x) + d.y * (0 - cent.y) + d.z * (0 - cent.z));
-    double c = (0 - cent.x) * (0 - cent.x) + (0 - cent.y) * (0 - cent.y) + (0 - cent.z) * (0 - cent.z) - 9;
+    double b = 2 * d.dot(cent * -1);
+    double c = (cent * -1).dot(cent * -1) - 4;
 
-    double t1 = -b + sqrt(b * b - 4 * c);
-    double t2 = -b - sqrt(b * b - 4 * c);
+    //double t1 = -b + sqrt(b * b - 4 * c);
+    //double t2 = -b - sqrt(b * b - 4 * c);
 
-    if (t1 > 0 || t2 > 0)
-        return true;
+    //if (t1 > 0 || t2 > 0)
+    //    return true;
+    
+    return ((b * b - 4 * c) >= 0);
 }
 
 Vec3 get_color(const Vec3& ray)
 {
     bool intersect = false;
 
-    for (Vec3 circle : circles)
-        if (is_intersect(circle, ray))
+    for (Vec3 * circle : circles)
+    {
+        if (is_intersect(*circle, ray))
         {
-            //intersect = true;
+            
+            intersect = true;
             break;
         }
+    }
 
     if (intersect)
         return Vec3(0.3, 0.2, 0.8);
@@ -57,19 +62,19 @@ int main()
     width = 800;
     height = 800;
 
-    // circles assumed to be contstant rad=3 for now
+    // circles contstant radius=2 for now
     // color (0.3, 0.2, 0.8) 
-    circles.push_back(Vec3(-1, -2, -8));
-    circles.push_back(Vec3(3, -2, -7));
+    circles.push_back(new Vec3(-10, -10, -10));
+    circles.push_back(new Vec3(-20, -20, -10));
 
-    std::ofstream file("test2.ppm");
+    std::ofstream file("test20.ppm");
     file << "P3\n" << width << ' ' << height << "\n255\n";
 
     for (int i = 0; i < width; i++) {
         for (int j = 0; j < height; j++) {
-            Vec3 ray = get_ray(i, j);
+            Vec3 ray = get_ray(i, j, width, height);
             Vec3 color = get_color(ray);
-            std::cout << "Pixel: " << i + j << " Ray (" << ray.x << ", " << ray.y << ", " << ray.z << ")" << std::endl;
+            //std::cout << "Pixel: " << i + j << " Ray (" << ray.x << ", " << ray.y << ", " << ray.z << ")" << std::endl;
             file << (int)(color.x * 255) << ' ' << (int)(color.y * 255) << ' ' << (int)(color.z * 255) << '\n';
         }
     }
