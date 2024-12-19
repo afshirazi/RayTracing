abbb4c115094329365de2994ecb989614a86c0f5
ali.shirazi@array.world <ali.shirazi@array.world>
2024-12-19 15:51:57 +0300

    test image crate

--- rust_tracer/src/main.rs
+++ rust_tracer/src/main.rs
@@ -1,3 +1,14 @@
+use image::{Rgb, RgbImage};
+
+
 fn main() {
-    println!("Hello, world!");
+    let img_buf = Vec::new();
+    let mut img = RgbImage::from_raw(1600, 900, img_buf).unwrap();
+
+    for (x, y, pixel) in img.enumerate_pixels_mut() {
+        *pixel = Rgb([x.rem_euclid(255) as u8, y.rem_euclid(255) as u8, (x+y).rem_euclid(255) as u8]);
+    }
+
+    img.save("test.png").unwrap();
 }
+
