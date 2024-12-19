87c000c09cf74548f1b35771152b7215a43f821d
ali.shirazi@array.world <ali.shirazi@array.world>
2024-12-19 15:54:41 +0300

    img works

--- rust_tracer/src/main.rs
+++ rust_tracer/src/main.rs
@@ -2,8 +2,7 @@ use image::{Rgb, RgbImage};
 
 
 fn main() {
-    let img_buf = Vec::new();
-    let mut img = RgbImage::from_raw(1600, 900, img_buf).unwrap();
+    let mut img = RgbImage::new(1600, 900);
 
     for (x, y, pixel) in img.enumerate_pixels_mut() {
         *pixel = Rgb([x.rem_euclid(255) as u8, y.rem_euclid(255) as u8, (x+y).rem_euclid(255) as u8]);
