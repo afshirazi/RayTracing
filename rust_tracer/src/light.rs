c52c32e77bd28b05aac38cf2bdc2f947c0f4fc7c
ali.shirazi@array.world <ali.shirazi@array.world>
2024-12-19 14:14:55 +0300

    implement light

---
+++ rust_tracer/src/light.rs
@@ -0,0 +1,3 @@
+pub mod light;
+
+pub use light::Light;
---
+++ rust_tracer/src/light/light.rs
@@ -0,0 +1,21 @@
+use crate::math::Vec3;
+
+pub struct Light {
+    pos: Vec3,
+    diff: Vec3,
+    spec: Vec3,
+}
+
+impl Light {
+    pub fn default_light() -> Light {
+        Light {
+            pos: Vec3::empty_vec(),
+            diff: Vec3::new(1.0, 1.0, 1.0),
+            spec: Vec3::new(1.0, 1.0, 1.0),
+        }
+    }
+
+    pub fn new(pos: Vec3, diff: Vec3, spec: Vec3) -> Light {
+        Light { pos, diff, spec }
+    }
+}
