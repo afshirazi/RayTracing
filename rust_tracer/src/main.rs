cceba578ac95be25cb66a2d92f762fe2655651a4
Ali Shirazi <afashirazi@proton.me>
2024-12-25 15:33:16 +0300

    test changes

--- rust_tracer/src/main.rs
+++ rust_tracer/src/main.rs
@@ -64,6 +64,7 @@ fn get_color(
     origin: &Vec3,
     objects: &Vec<Object>,
     lights: &Vec<Light>,
+    src_obj: Option<&Object>,
     depth: u8,
 ) -> Vec3 {
     let mut color_buf = Vec3::new(0.0, 0.0, 0.0);
@@ -104,22 +105,24 @@ fn get_color(
         let light_dir = (&light.pos - &intr_point).norm();
         let light_refl = (&(2.0 * (light_dir.dot(&normal)) * &normal) - &light_dir).norm();
 
+        let light_intensity = Vec3::euclid_dist_sq(&light.pos, &intr_point).recip();
+
         let diffuse_term = light_dir.dot(&normal); // doubles to check if light is on correct side of object
         let spec_term = (origin - &intr_point).norm().dot(&light_refl);
         if diffuse_term > 0.0 {
-            color_buf.x += intr_obj.get_diff().x * diffuse_term * light.diff.x
-                + intr_obj.get_spec().x * spec_term.powf(intr_obj.get_shin()) * light.spec.x;
-            color_buf.y += intr_obj.get_diff().y * diffuse_term * light.diff.y
-                + intr_obj.get_spec().y * spec_term.powf(intr_obj.get_shin()) * light.spec.y;
-            color_buf.z += intr_obj.get_diff().z * diffuse_term * light.diff.z
-                + intr_obj.get_spec().z * spec_term.powf(intr_obj.get_shin()) * light.spec.z;
+            color_buf.x += intr_obj.get_diff().x * diffuse_term * light.diff.x * light_intensity
+                + intr_obj.get_spec().x * spec_term.powf(intr_obj.get_shin()) * light.spec.x * light_intensity;
+            color_buf.y += intr_obj.get_diff().y * diffuse_term * light.diff.y * light_intensity
+                + intr_obj.get_spec().y * spec_term.powf(intr_obj.get_shin()) * light.spec.y * light_intensity;
+            color_buf.z += intr_obj.get_diff().z * diffuse_term * light.diff.z * light_intensity
+                + intr_obj.get_spec().z * spec_term.powf(intr_obj.get_shin()) * light.spec.z * light_intensity;
         }
     }
 
     if depth > 0 && intr_obj.get_spec().dot(intr_obj.get_spec()) > 0.3 {
         let in_ray = origin - &intr_point;
         let refl_ray = (&(2.0 * (in_ray.dot(&normal)) * &normal) - &in_ray).norm();
-        color_buf = color_buf + get_color(&refl_ray, &intr_point, objects, lights, depth - 1);
+        color_buf = color_buf + get_color(&refl_ray, &intr_point, objects, lights, Some(intr_obj), depth - 1);
     }
 
     color_buf
@@ -165,7 +168,7 @@ fn main() {
 
     for (x, y, pixel) in img.enumerate_pixels_mut() {
         let ray = get_ray(&eye, x, y, 1600, 900);
-        let px_color = get_color(&ray, &eye, &objs, &lights);
+        let px_color = get_color(&ray, &eye, &objs, &lights, None, 0);
         *pixel = Rgb([
             (px_color.x.clamp(0.0, 1.0) * 255.0) as u8,
             (px_color.y.clamp(0.0, 1.0) * 255.0) as u8,
