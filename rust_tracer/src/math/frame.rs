ceebe58686281d82938ccb1a9f181d6471ec6c68
Ali Shirazi <afashirazi@proton.me>
2025-12-30 17:05:39 +0300

    make frame

---
+++ rust_tracer/src/math/frame.rs
@@ -0,0 +1,23 @@
+use crate::math::Vec3;
+
+/// x, y, z must be orthonormal
+//TODO: can add check for orthonormality in debug builds, like PBRT
+pub struct Frame {
+    x: Vec3,
+    y: Vec3,
+    z: Vec3,
+} 
+
+impl Frame {
+    pub fn new(x: Vec3, y: Vec3, z: Vec3) -> Self {
+        Self { x, y, z }
+    }
+
+    pub fn to_local(&self, v: Vec3) -> Vec3 {
+        Vec3::new(v.dot(&self.x), v.dot(&self.y), v.dot(&self.z))
+    }
+
+    pub fn from_local(&self, v: Vec3) -> Vec3 {
+        v.x * &self.x + v.y * &self.y + v.z * &self.z
+    }
+}
