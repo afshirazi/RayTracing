48883083e1d97f3daed45d8a1843cf8ea78e36d5
Ali Shirazi <afashirazi@proton.me>
2025-12-31 18:55:33 +0300

    impl bxdf and change it a lil

--- rust_tracer/src/bxdf.rs
+++ rust_tracer/src/bxdf.rs
@@ -1,9 +1,10 @@
-use crate::{bxdf::diffuse_bxdf::DiffuseBxdf, math::Vec3};
+use crate::math::{Frame, Vec3};
 
 pub mod diffuse_bxdf;
 
 pub struct Bsdf<T: Bxdf> {
     bxdf: T,
+    frame: Frame,
 }
 
 pub struct BsdfSample {
@@ -15,9 +16,35 @@ pub struct BsdfSample {
 // may add TransportMode later
 
 pub trait Bxdf {
-    fn f(w_o: Vec3, w_i: Vec3) -> Vec3; // gonna change to sampled spectrum once I figure that out
-    fn sample_f(w_o: Vec3, u: f32, uc: (f32, f32)) -> BsdfSample;
+    fn f(&self, w_o: &Vec3, w_i: &Vec3) -> Vec3; // gonna change to sampled spectrum once I figure that out
+    fn sample_f(&self, w_o: &Vec3, u: f32, uc: (f32, f32)) -> Option<BsdfSample>;
     //fn pdf();
 }
 
+impl<T: Bxdf> Bsdf<T> {
+    pub fn new(normal: Vec3, dpdus: Vec3, bxdf: T) -> Self {
+        Self {
+            bxdf,
+            frame: Frame::from_xz(dpdus.norm(), normal),
+        }
+    }
+}
+
+impl<T: Bxdf> Bxdf for Bsdf<T> {
+    fn f(&self, w_o: &Vec3, w_i: &Vec3) -> Vec3 {
+        let local_w_o = self.frame.to_local(w_o);
+        let local_w_i = self.frame.to_local(w_i);
+        self.bxdf.f(&local_w_o, &local_w_i)
+    }
+
+    fn sample_f(&self, w_o: &Vec3, u: f32, uc: (f32, f32)) -> Option<BsdfSample> {
+        let local_w_o = self.frame.to_local(w_o);
+        let mut bs = self.bxdf.sample_f(&local_w_o, u, uc)?;
+        if bs.pdf == 0.0 || bs.w_i.z == 0.0 {
+            return None
+        }
+        bs.w_i = self.frame.from_local(&bs.w_i);
+        Some(bs)
+    }
+}
 
