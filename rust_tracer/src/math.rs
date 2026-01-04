fd0ae55da600480454d7501b38b07b4249c0eb03
Ali Shirazi <afashirazi@proton.me>
2026-01-05 00:23:40 +0300

    pub fns

--- rust_tracer/src/math.rs
+++ rust_tracer/src/math.rs
@@ -1,12 +1,12 @@
-mod vec3;
 mod frame;
+mod vec3;
 
 use std::f32;
 
-pub use vec3::Vec3;
 pub use frame::Frame;
+pub use vec3::Vec3;
 
-fn sample_uniform_disk_concentric(u: (f32, f32)) -> (f32, f32) {
+pub fn sample_uniform_disk_concentric(u: (f32, f32)) -> (f32, f32) {
     let u_offset = (2.0 * u.0 - 1.0, 2.0 * u.1 - 1.0); // map to [-1, 1]^2
     if u_offset == (0.0, 0.0) {
         return u_offset;
@@ -23,12 +23,12 @@ fn sample_uniform_disk_concentric(u: (f32, f32)) -> (f32, f32) {
     (r * theta.cos(), r * theta.sin())
 }
 
-fn sample_cosine_hemisphere(u: (f32, f32)) -> Vec3 {
+pub fn sample_cosine_hemisphere(u: (f32, f32)) -> Vec3 {
     let d = sample_uniform_disk_concentric(u);
     let z = (1.0 - d.0 * d.0 - d.1 * d.1).sqrt().max(0.0);
     Vec3::new(d.0 as f64, d.1 as f64, z as f64)
 }
 
-fn cosine_hemisphere_pdf(cos_theta: f32) -> f32 {
+pub fn cosine_hemisphere_pdf(cos_theta: f32) -> f32 {
     cos_theta * f32::consts::FRAC_1_PI
 }
