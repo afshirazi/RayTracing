19ecbe1d0a88c41d20df90202abcbcafc99e94db
Ali Shirazi <afashirazi@proton.me>
2026-01-31 21:29:50 +0300

    update sampler

--- rust_tracer/src/sampler.rs
+++ rust_tracer/src/sampler.rs
@@ -1,3 +1,6 @@
+pub type Sample2d = (f32, f32); // TODO: maybe use Point2f like pbrt does, or something else, but refactor under math when needed
+
 pub trait Sampler {
-    // TODO:
+    fn get_1d() -> f32;
+    fn get_2d() -> Sample2d;
 }
