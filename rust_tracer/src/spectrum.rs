587eb26196fecd872dce88e8bccdedbd171474bb
Ali Shirazi <afashirazi@proton.me>
2026-03-06 00:49:02 +0300

    spectrum

--- rust_tracer/src/spectrum.rs
+++ rust_tracer/src/spectrum.rs
@@ -1,5 +1,9 @@
 use std::ops::Index;
 
+// pbrt defines these so I will too :)
+const MIN_LAMBDA: f32 = 360.0;
+const MAX_LAMBDA: f32 = 830.0;
+
 pub trait Spectrum : Index<usize> {
     fn max_value(&self) -> f32;
 }
