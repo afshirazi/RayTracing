7cf05d19aca7f99c181869d61ea41a27e5be2985
Ali Shirazi <afashirazi@proton.me>
2026-03-27 19:22:19 +0300

    light interface

--- rust_tracer/src/light.rs
+++ rust_tracer/src/light.rs
@@ -1,3 +1,57 @@
 mod point_light;
 
 pub use point_light::PointLight;
+
+use crate::{
+    math::Vec3,
+    spectrum::sampled_spectrum::{SampledSpectrum, SampledWavelengths},
+};
+
+pub trait Light {
+    fn phi(&self, lambdas: &SampledWavelengths) -> SampledSpectrum;
+    fn ltype(&self) -> LightType;
+    fn sample_li(&self, ctx: &LightSampleContext) -> Option<LightLiSample>;
+    fn pdf_li(&self, ctx: &LightSampleContext, w_i: Vec3) -> f32;
+    /// Also referred to as L().
+    // TODO: define point 2D and 3D and vec 2d?
+    fn radiance(&self, point: &Vec3, normal: &Vec3, uv: (f32, f32), w: &Vec3, lambdas: &SampledWavelengths) -> SampledSpectrum;
+    // fn le(&self, origin: Vec3, dir: Vec3, lambdas: &SampledWavelengths) -> SampledSpectrum;
+}
+
+pub struct LightSampleContext {
+    pub point: Vec3,
+    pub normal: Vec3,
+}
+
+impl LightSampleContext {
+    pub fn new(point: Vec3, normal: Vec3) -> Self {
+        Self { point, normal }
+    }
+}
+
+pub struct LightLiSample {
+    pub radiance: SampledSpectrum,
+    pub w_i: Vec3,
+    pub pdf: f32,
+}
+
+impl LightLiSample {
+    pub fn new(radiance: SampledSpectrum, w_i: Vec3, pdf: f32) -> Self {
+        Self { radiance, w_i, pdf }
+    }
+}
+
+pub enum LightType {
+    DeltaPosition,
+    DeltaDirection,
+    Area,
+}
+
+impl LightType {
+    pub fn is_delta(&self) -> bool {
+        match self {
+            Self::DeltaDirection | Self::DeltaPosition => true,
+            _ => false,
+        }
+    }
+}
