c8256d8b28bd512896b497c6eb89c844bf2cfd69
Ali Shirazi <afashirazi@proton.me>
2026-04-20 23:56:11 +0300

    mat3 inverse and determinant

--- rust_tracer/src/math/matrix.rs
+++ rust_tracer/src/math/matrix.rs
@@ -31,18 +31,44 @@ impl Matrix<4> {
         todo!()
     }
 
-    pub fn inverse(&self) -> Self {
+    pub fn inverse(&self) -> Option<Self> {
         todo!()
     }
 }
 
 impl Matrix<3> {
     pub fn determinant(&self) -> f32 {
-        todo!()
+        let minor_12 = difference_of_products(self[1][1], self[2][2], self[1][2], self[2][1]);
+        let minor_02 = difference_of_products(self[1][0], self[2][2], self[1][2], self[2][0]);
+        let minor_01 = difference_of_products(self[1][0], self[2][1], self[1][1], self[2][0]);
+
+        f32::mul_add(
+            self[0][0],
+            minor_12,
+            difference_of_products(self[0][2], minor_01, -self[0][1], minor_02),
+        )
     }
 
-    pub fn inverse(&self) -> Self {
-        todo!()
+    pub fn inverse(&self) -> Option<Self> {
+        let det = self.determinant();
+        if det == 0.0 {
+            return None;
+        }
+
+        let inv_det = det.inv();
+        let mut r = [[0.0; 3]; 3];
+
+        r[0][0] = inv_det * difference_of_products(self[1][1], self[2][2], self[1][2], self[2][1]);
+        r[1][0] = inv_det * difference_of_products(self[1][2], self[2][0], self[1][0], self[2][2]);
+        r[2][0] = inv_det * difference_of_products(self[1][0], self[2][1], self[1][1], self[2][0]);
+        r[0][1] = inv_det * difference_of_products(self[0][2], self[2][1], self[0][1], self[2][2]);
+        r[1][1] = inv_det * difference_of_products(self[0][0], self[2][2], self[0][2], self[2][0]);
+        r[2][1] = inv_det * difference_of_products(self[0][1], self[2][0], self[0][0], self[2][1]);
+        r[0][2] = inv_det * difference_of_products(self[0][1], self[1][2], self[0][2], self[1][1]);
+        r[1][2] = inv_det * difference_of_products(self[0][2], self[1][0], self[0][0], self[1][2]);
+        r[2][2] = inv_det * difference_of_products(self[0][0], self[1][1], self[0][1], self[1][0]);
+
+        Some(Self::new(r))
     }
 }
 
@@ -54,7 +80,7 @@ impl Matrix<2> {
     pub fn inverse(&self) -> Option<Self> {
         let det = self.determinant();
         if det != 0.0 {
-            let tmp_mat = Matrix::new([[self[1][1], -self[0][1]],[-self[1][0], self[0][0]]]);
+            let tmp_mat = Matrix::new([[self[1][1], -self[0][1]], [-self[1][0], self[0][0]]]);
             let inv_mat = tmp_mat * det.inv();
             Some(inv_mat)
         } else {
@@ -76,7 +102,6 @@ impl Matrix<1> {
         }
     }
 }
-
 
 /// Generic matrix-vector multiply.
 pub fn mul<R, T, const N: usize>(mat: &Matrix<N>, v: &T) -> R
