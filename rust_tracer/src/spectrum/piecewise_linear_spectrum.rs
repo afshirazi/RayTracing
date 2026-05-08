88608d0ff322980ce22d2ddcd198316ae6eaaaac
Ali Shirazi <afashirazi@proton.me>
2026-05-08 15:07:53 +0300

    linear interpolated spectrum

--- rust_tracer/src/spectrum/piecewise_linear_spectrum.rs
+++ rust_tracer/src/spectrum/piecewise_linear_spectrum.rs
@@ -1,6 +1,4 @@
-use std::ops::Index;
-
-use crate::{math::lerp, spectrum::Spectrum};
+use crate::{math::lerp, spectrum::{N_SPECTRUM_SAMPLES, Spectrum}};
 
 use super::sampled_spectrum::{SampledSpectrum, SampledWavelengths};
 
@@ -20,14 +18,28 @@ impl PiecewiseLinearSpectrum {
 
 impl Spectrum for PiecewiseLinearSpectrum {
     fn max_value(&self) -> f32 {
-        todo!()
+        *self.values.iter().max_by(|a, b| a.total_cmp(b)).unwrap_or(&0.0)
     }
 
     fn sample(&self, lambdas: &SampledWavelengths) -> SampledSpectrum {
-        todo!()
+        let mut s = SampledSpectrum::filled(0.0);
+        for idx in 0..N_SPECTRUM_SAMPLES {
+            s[idx] = self.index(lambdas[idx]);
+        }
+        s
     }
     
     fn index(&self, lambda: f32) -> f32 {
-        todo!()
+        if self.lambdas.is_empty() || lambda < *self.lambdas.first().unwrap() || lambda > *self.lambdas.last().unwrap() {
+            0.0
+        } else {
+            self.lambdas.windows(2)
+            .find(|interval| interval[0] <= lambda && interval[1] >= lambda)
+            .map(|interval| {
+                let u = (lambda - interval[0]) / (interval[1] - interval[0]);
+                lerp(u, interval[0], interval[1])
+            })
+            .unwrap()
+        }
     }
 }
